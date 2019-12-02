0ffd3338482ee085d3464f35bb78c973fe3d06fa
Barkarula <kliment.barkalov@gmail.com>
2019-12-02 04:17:18 +0300

    Updates

---
+++ precache-manifest.f4b574d0f669eb5e1331936a51c59891.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9654be8804b5e06f15e4",
+    "url": "/atwTest/static/css/main.cc6716a9.chunk.css"
+  },
+  {
+    "revision": "9654be8804b5e06f15e4",
+    "url": "/atwTest/static/js/main.35b567f9.chunk.js"
+  },
+  {
+    "revision": "c8cded91ee13666580f6",
+    "url": "/atwTest/static/js/runtime~main.9bbdea95.js"
+  },
+  {
+    "revision": "c62893a1cb0547e146b2",
+    "url": "/atwTest/static/css/2.b5334491.chunk.css"
+  },
+  {
+    "revision": "c62893a1cb0547e146b2",
+    "url": "/atwTest/static/js/2.81aab18c.chunk.js"
+  },
+  {
+    "revision": "2da46147a19449255266514c196ca9cc",
+    "url": "/atwTest/static/media/readed.2da46147.svg"
+  },
+  {
+    "revision": "1456f01338efe100733984d48807dd08",
+    "url": "/atwTest/static/media/noreaded.1456f013.svg"
+  },
+  {
+    "revision": "dd36efef3de2a0c20c53b2aaaa2ffb44",
+    "url": "/atwTest/index.html"
+  }
+];
